b482b205d689044ec5bbee62088cbea9cadbf203
edemots <eliot@demots.fr>
2019-06-19 15:12:15 +0200

    User registration/login

---
+++ src/resources/lang/fr/auth.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'before_proceeding_verify_email' => 'Avant de continuer, veuillez vérifier que vous n\'avez pas reçu de lien de vérification.',
+    'didnt_received_link' => 'Si vous n\'avez pas reçu l\'e-mail',
+    'email' => 'Adresse e-mail',
+    'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
+    'forgot_your_password' => 'Mot de passe oublié ?',
+    'fresh_verification_link' => 'Un lien de vérification a été envoyé à votre adresse e-mail',
+    'login' => 'Connexion',
+    'logout' => 'Déconnexion',
+    'name' => 'Nom',
+    'password' => 'Mot de passe',
+    'confirm_password' => 'Confirmez le mot de passe',
+    'register' => 'Inscription',
+    'remember_me' => 'Se souvenir de moi',
+    'request_new_link' => 'Cliquez-ici pour en recevoir un nouveau',
+    'reset_password' => 'Réinitialiser le mot de passe',
+    'send_password_reset_link' => 'Envoyer le lien de réinitialisation du mot de passe',
+    'throttle' => 'Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.',
+    'verify_email' => 'Verifiez votre adresse e-mail',
+
+];
--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -1,77 +1,60 @@
-@extends('layouts.app')
+@extends('layouts.hero')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <div class="columns is-centered">
+        <article class="card column is-8-tablet is-6-desktop">
+            <div class="card-content">
+                <h1 class="title is-1 has-text-black">{{ __('auth.register') }}</h1>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                    <div class="field">
+                        <label for="name" class="label">{{ __('auth.name') }}</label>
+                        <div class="control">
+                            <input id="name" type="text" class="input{{ $errors->has('name') ? ' is-danger' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                            @if ($errors->has('name'))
+                                <p class="help is-danger" role="alert">{{ $errors->first('name') }}</p>
+                            @endif
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                    <div class="field">
+                        <label for="email" class="label">{{ __('auth.email') }}</label>
+                        <div class="control">
+                            <input id="email" type="email" class="input{{ $errors->has('email') ? ' is-danger' : '' }}" name="email" value="{{ old('email') }}" required>
+                            @if ($errors->has('email'))
+                                <p class="help is-danger" role="alert">{{ $errors->first('email') }}</p>
+                            @endif
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                    <div class="field">
+                        <label for="password" class="label">{{ __('auth.password') }}</label>
+                        <div class="control">
+                            <input id="password" type="password" class="input{{ $errors->has('password') ? ' is-danger' : '' }}" name="password" required>
+                            @if ($errors->has('password'))
+                                <p class="help is-danger" role="alert">{{ $errors->first('password') }}</p>
+                            @endif
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
+                    <div class="field">
+                        <label for="password-confirm" class="label">{{ __('auth.confirm_password') }}</label>
+                        <div class="control">
+                            <input id="password-confirm" type="password" class="input" name="password_confirmation" required>
                         </div>
+                    </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
+                    <div class="field">
+                        <div class="control">
+                            <button type="submit" class="button is-info">{{ __('auth.register') }}</button>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
-        </div>
+        </article>
     </div>
 </div>
 @endsection
