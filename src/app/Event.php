02497af36f00e65200ebafaa40a234623ca34f26
Treyssat-Vincent Nino <nino+git@treyssatvincent.fr>
2019-07-02 16:47:28 +0200

    add: link event to users

--- src/app/Event.php
+++ src/app/Event.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    public function subscribers()
+    {
+        return $this->belongsToMany(User::class, 'users_events', 'event_id', 'user_id');
+    }
 }
